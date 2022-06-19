b5f3a92e3ea1ad63ce27fdf2ae56da691cbca3b5
prajnapurnanggara <gwmiilo@gmail.com>
2022-06-19 14:00:32 +0800

    dashboard guru

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,20 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DashboardGuru from '../views/guru/Dashboard.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/dashboardguru',
+    name: 'dashboardguru',
+    component: DashboardGuru
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = createRouter({
