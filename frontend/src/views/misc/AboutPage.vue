fd7402411fc59d215b9cd9687e5e5e3fb0c46049
Maharta <gedemaharta.dev@gmail.com>
2022-06-20 10:35:24 +0800

    added AboutView and AboutHeader

---
+++ frontend/src/views/misc/AboutPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <about-header></about-header>
+</template>
+
+<style scoped></style>
+
+<script>
+import AboutHeader from "../../components/about/AboutHeader.vue";
+export default {
+  components: {
+    AboutHeader,
+  },
+};
+</script>
