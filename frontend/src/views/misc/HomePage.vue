a0ceeb6d24d212027f7ac24c5e260be24917fc81
Maharta <gedemaharta.dev@gmail.com>
2022-06-22 15:35:36 +0800

    added home page

---
+++ frontend/src/views/misc/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <the-header></the-header>
+  <home-description></home-description>
+  <feature-section></feature-section>
+</template>
+
+<script>
+import TheHeader from '@/components/TheHeader.vue';
+import HomeDescription from '@/components/home/HomeDescription.vue';
+import FeatureSection from '@/components/home/FeatureSection.vue';
+export default {
+  components: {
+    TheHeader,
+    HomeDescription,
+    FeatureSection,
+  },
+};
+</script>
