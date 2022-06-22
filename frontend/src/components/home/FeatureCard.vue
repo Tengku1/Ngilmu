027682c1393960c08ad0621c1fa7446134bd3fe7
Maharta <gedemaharta.dev@gmail.com>
2022-06-22 15:35:56 +0800

    added FeatureCard components and Feature Section

---
+++ frontend/src/components/home/FeatureCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="feature__card">
+    <slot></slot>
+    <h3>{{ featureName }}</h3>
+    <slot name="description"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    featureName: {
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.feature__card {
+  border-radius: 16px;
+  background-color: white;
+  padding: 50px;
+  margin-right: 24px;
+  margin-left: 24px;
+}
+</style>
---
+++ frontend/src/components/home/FeatureSection.vue
@@ -0,0 +1,74 @@
+<template>
+  <section>
+    <p>Discover</p>
+    <h2>Our Features</h2>
+    <div class="grid">
+      <feature-card featureName="Temukan Guru">
+        <template #default
+          ><i class="fa-solid fa-person-chalkboard"></i
+        ></template>
+        <template #description
+          >Temukan berbagai guru yang kompeten di bidangnya</template
+        >
+      </feature-card>
+      <feature-card featureName="Temukan Guru">
+        <template #default
+          ><i class="fa-solid fa-person-chalkboard"></i
+        ></template>
+        <template #description
+          >Temukan berbagai guru yang kompeten di bidangnya</template
+        >
+      </feature-card>
+      <feature-card featureName="Temukan Guru">
+        <template #default
+          ><i class="fa-solid fa-person-chalkboard"></i
+        ></template>
+        <template #description
+          >Temukan berbagai guru yang kompeten di bidangnya</template
+        >
+      </feature-card>
+      <feature-card featureName="Temukan Guru">
+        <template #default
+          ><i class="fa-solid fa-person-chalkboard"></i
+        ></template>
+        <template #description
+          >Temukan berbagai guru yang kompeten di bidangnya</template
+        >
+      </feature-card>
+    </div>
+  </section>
+</template>
+
+<script>
+import FeatureCard from './FeatureCard.vue';
+export default {
+  components: {
+    FeatureCard,
+  },
+};
+</script>
+
+<style scoped>
+section {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+}
+
+.grid {
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  row-gap: 2rem;
+}
+
+@media screen and (min-width: 780px) {
+  .grid {
+    grid-template-columns: repeat(2, minmax(330px, 1fr));
+    grid-template-rows: repeat(2, 1fr);
+  }
+}
+</style>
