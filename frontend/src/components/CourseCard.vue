f0f6cc8115f97912d701ae013cfda54a33d2e613
Maharta <gedemaharta.dev@gmail.com>
2022-07-03 00:02:51 +0800

    added CourseCard component for dynamic v-for

---
+++ frontend/src/components/CourseCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <h4>{{ name }}</h4>
+      <div class="card-header-action">
+        <router-link class="btn btn-primary" to="/murid/detailcourse"
+          ><i class=""></i>Detail</router-link
+        >
+      </div>
+    </div>
+    <div class="card-body">
+      <div class="chocolat-parent">
+        <a
+          href="assets/img/example-image.jpg"
+          class="chocolat-image"
+          title="Just an example"
+        >
+          <div
+            data-crop-image="285"
+            style="overflow: hidden; position: relative; height: 285px"
+          >
+            <img alt="image" :src="imageUrl" class="img-fluid" />
+          </div>
+        </a>
+      </div>
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['imageUrl', 'description', 'name', 'id'],
+};
+</script>
