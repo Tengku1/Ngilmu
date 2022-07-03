4c622a20b43564903f607438643e186b1c46bf2b
Maharta <gedemaharta.dev@gmail.com>
2022-07-03 23:20:51 +0800

    changed name for navbar

--- frontend/src/components/guru/Navbar.vue
+++ frontend/src/components/guru/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
-      <div class="navbar-bg"></div>
-      <nav class="navbar navbar-expand-lg main-navbar">
-        <form class="form-inline mr-auto">
-          <ul class="navbar-nav mr-3">
-            <!-- <li>
+  <div class="navbar-bg"></div>
+  <nav class="navbar navbar-expand-lg main-navbar">
+    <form class="form-inline mr-auto">
+      <ul class="navbar-nav mr-3">
+        <!-- <li>
               <a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"
                 ><i class="fas fa-bars"></i
               ></a>
@@ -16,29 +16,32 @@
                 ><i class="fas fa-search"></i
               ></a>
             </li> -->
-          </ul>
-        </form>
-        <ul class="navbar-nav navbar-right">
-          <li class="dropdown">
-            <a
-              href="#"
-              data-toggle="dropdown"
-              class="nav-link dropdown-toggle nav-link-lg nav-link-user"
-            >
-              <img
-                alt="image"
-                src="@/assets/img/avatar/avatar-1.png"
-                class="rounded-circle mr-1"
-              />
-              <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div></a
-            >
-            <div class="dropdown-menu dropdown-menu-right">
-            <router-link class="dropdown-item has-icon text-danger" to="/guru/login"
-              ><i class="fas fa-sign-out-alt"></i>Logout</router-link>
-            </div>
-          </li>
-        </ul>
-      </nav>
+      </ul>
+    </form>
+    <ul class="navbar-nav navbar-right">
+      <li class="dropdown">
+        <a
+          href="#"
+          data-toggle="dropdown"
+          class="nav-link dropdown-toggle nav-link-lg nav-link-user"
+        >
+          <img
+            alt="image"
+            src="@/assets/img/avatar/avatar-1.png"
+            class="rounded-circle mr-1"
+          />
+          <div class="d-sm-none d-lg-inline-block">Hi, Tengku</div></a
+        >
+        <div class="dropdown-menu dropdown-menu-right">
+          <router-link
+            class="dropdown-item has-icon text-danger"
+            to="/guru/login"
+            ><i class="fas fa-sign-out-alt"></i>Logout</router-link
+          >
+        </div>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
@@ -47,5 +50,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
