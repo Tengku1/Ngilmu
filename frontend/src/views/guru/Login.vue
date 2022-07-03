6fc26be0b7d097c7b59e70935f6989d67c38e992
Tengku1 <anountengku@gmail.com>
2022-07-03 23:21:27 +0800

    fix url register for teachers

--- frontend/src/views/guru/Login.vue
+++ frontend/src/views/guru/Login.vue
@@ -2,9 +2,7 @@
   <div id="app">
     <section class="section">
       <div class="d-flex flex-wrap align-items-stretch">
-        <div
-          class="col-lg-4 col-md-6 col-12 order-lg-1 min-vh-100 order-2 bg-white"
-        >
+        <div class="col-lg-4 col-md-6 col-12 order-lg-1 min-vh-100 order-2 bg-white">
           <div class="p-4 m-3">
             <h4 class="text-dark font-weight-normal">
               Selamat Datang di <span class="font-weight-bold">Ngilmu</span>
@@ -17,16 +15,8 @@
             <form @submit.prevent class="needs-validation" novalidate="">
               <div class="form-group">
                 <label for="email">Email</label>
-                <input
-                  id="email"
-                  type="text"
-                  class="form-control"
-                  name="email"
-                  tabindex="1"
-                  required=""
-                  autofocus=""
-                  v-model="email"
-                />
+                <input id="email" type="text" class="form-control" name="email" tabindex="1" required="" autofocus=""
+                  v-model="email" />
                 <div class="invalid-feedback">Please fill in your email</div>
               </div>
 
@@ -34,24 +24,13 @@
                 <div class="d-block">
                   <label for="password" class="control-label">Password</label>
                 </div>
-                <input
-                  id="password"
-                  type="password"
-                  class="form-control"
-                  name="password"
-                  tabindex="2"
-                  required=""
-                  v-model="password"
-                />
+                <input id="password" type="password" class="form-control" name="password" tabindex="2" required=""
+                  v-model="password" />
                 <div class="invalid-feedback">please fill in your password</div>
               </div>
 
               <div class="form-group text-right">
-                <button
-                  class="btn btn-primary btn-lg btn-icon icon-right"
-                  tabindex="4"
-                  @click="login(email, password)"
-                >
+                <button class="btn btn-primary btn-lg btn-icon icon-right" tabindex="4" @click="login(email, password)">
                   Masuk
                 </button>
               </div>
@@ -63,9 +42,7 @@
               </div>
               <div class="mt-2 text-center">
                 Belum memiliki akun?
-                <router-link class="text" to="/murid/register"
-                  >Buat akunmu disini</router-link
-                >
+                <router-link class="text" to="/guru/register">Buat akunmu disini</router-link>
               </div>
             </form>
           </div>
@@ -73,8 +50,7 @@
         <div
           class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom"
           data-background="https://images.unsplash.com/photo-1510797215324-95aa89f43c33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80"
-          style="background-image: url('../assets/img/unsplash/login-bg.jpg')"
-        >
+          style="background-image: url('../assets/img/unsplash/login-bg.jpg')">
           <div class="absolute-bottom-left index-2">
             <div class="text-light p-5 pb-2">
               <div class="mb-5 pb-3">
@@ -84,19 +60,9 @@
                 </h5>
               </div>
               Photo by
-              <a
-                class="text-light bb"
-                target="_blank"
-                href="@/assets/img/avatar/avatar-1.png"
-                >Justin Kauffman</a
-              >
+              <a class="text-light bb" target="_blank" href="@/assets/img/avatar/avatar-1.png">Justin Kauffman</a>
               on
-              <a
-                class="text-light bb"
-                target="_blank"
-                href="https://unsplash.com"
-                >Unsplash</a
-              >
+              <a class="text-light bb" target="_blank" href="https://unsplash.com">Unsplash</a>
             </div>
           </div>
         </div>
@@ -123,4 +89,5 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+</style>
